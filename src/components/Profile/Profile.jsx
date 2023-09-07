bb483c83b2184eeb1627e45a17ca2d6d7b73dbe0
yolovi <yolandalopevi@gmail.com>
2023-09-07 16:55:32 +0200

    vite.config changet to define backend for images route, modified profile component src images

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,9 @@ const Profile = () => {
     const { user, isLoading } = useSelector((state) => state.auth);
 
 
-  const { username, email, followers } = user;
+  const { username, email, followers, avatar_url} = user;
+  console.log(user)
+  console.log(avatar_url)
   console.log(username);
 
   // const postsUser = postIds.map(post => ({
@@ -31,9 +33,10 @@ const Profile = () => {
       <div>Profile</div>
       <div>
         <h1>{username}</h1>
-        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+        {/* <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img> */}
+        <img alt="avatar-profile-image" src={avatar_url}></img>
         <p>Email: {email}</p>
-        <p>Followers: {followers}</p>
+        <p>Followers: {followers?.length}</p>
         <p>Following: hay que hacer la logica en bakcend</p>
         <p>posts hacer map o traer de componente</p>
       </div>
@@ -43,10 +46,12 @@ const Profile = () => {
 
 export default Profile;
 
-
+// cover={<img alt="image-lamp" src={product.image_url} />} // Cargar la imagen usando la URL
 
 /*//TODO: multer foto perfil en bakcend en user loged, si que hay en register */
 /*//TODO: Following hay que hacer la logica en bakcend */
+/*//TODO: Followers hay que hacer la logica en bakcend */
 /*//FIXME: Post no salen en el post, funciona en backend? probado en postman no va*/
+
 
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+    //para servir las imágenes del backend config Vite
+    server: {
+      proxy: {
+        // Redirigir solicitudes de imágenes al servidor backend en el puerto 3000
+        '/assets/images': 'http://localhost:3000',
+      },
+    },
+  })
+  
+
